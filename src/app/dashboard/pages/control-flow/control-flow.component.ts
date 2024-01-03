e05e5a08fe243c07d0fc78ea924757624c84fd02
daan98 <daan98.da@gmail.com>
2024-01-03 00:53:18 -0600

    New ng if, ng switch and ng for applied.

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
+type Grade = 'A' | 'B' | 'C' | 'D' | 'F';
 @Component({
   selector: 'app-control-flow',
   standalone: true,
@@ -8,5 +9,15 @@ import { Component } from '@angular/core';
   styleUrl: './control-flow.component.css'
 })
 export default class ControlFlowComponent {
+  public showContent = signal<boolean>(false);
+  public grades = signal<Grade>('B');
 
+  public toggleContent() : void {
+    this.showContent.update(( value ) => !value);
+  }
+
+  public getGradeMessage(grade : any) : void {
+    console.log({grade})
+    // this.grades.set()
+  }
 }
